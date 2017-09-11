f9cf163652436055d2ae59784e51dc6501c7f7e6
Minh <minhnguyen1995@googlemail.com>
2017-09-11 23:17:00 +0100

    !

--- src/Min.hs
+++ src/Min.hs
@@ -19,17 +19,19 @@ import Debug.Trace
 import SDL.Vect
 import qualified SDL as SDL
 import System.FilePath.Posix
+import System.Random
 import Control.Monad.State as ST
 import qualified SDL.Raw.Timer as Raw
 import qualified SDL.Raw.Types as Raw
 
 screenWidth, screenHeight :: CInt
-(screenWidth, screenHeight) = (640, 480)
+(screenWidth, screenHeight) = (800, 800)
+
+
 -- SDL.Rectangle  = Point (x,y) -> (w,h) -> Rectangle Int
 -- SDL.Rectangle :: Point V2 a -> V2 a -> SDL.Rectangle a
 -- P  :: f a -> Point f a
 -- V2 :: a -> a -> V2 a
-
 
 -- SDL.Texture, (Width, Height)
 data Texture = Texture SDL.Texture (V2 CInt)
@@ -37,12 +39,11 @@ data Character = Character Texture CharState
 data Game = Game { getTime :: Int } 
 type Time = Int
 
-newtype Sprite = Sprite {runSprite :: CharSTATE -> (Texture, [SDL.Rectangle CInt])} 
+newtype Sprite = Sprite {runSprite :: Direction -> (Texture, [SDL.Rectangle CInt])} 
 type SpriteIndex = Int
-data CharSTATE = U | D | L | R 
-type CharState = ((Sprite, CharSTATE, SpriteIndex, Game), Body)
+data Direction = U | D | L | R 
+type CharState = ((Sprite, Direction, SpriteIndex, Game), Body)
 type Body = [(CInt, CInt)]
-type ElapsedTime = Int
 
 updateState :: Char -> State CharState Body
 updateState c = do
@@ -83,7 +84,7 @@ loadTexture r filePath = do
   SDL.freeSurface surface
   return (Texture t size)
 
-createSprite :: CharSTATE -> Texture -> [SDL.Rectangle CInt] -> Sprite
+createSprite :: Direction -> Texture -> [SDL.Rectangle CInt] -> Sprite
 createSprite s t xs = Sprite $ \s -> (t, xs)
 
 
@@ -114,31 +115,38 @@ processKeyboard' state keySym ticks =
                _ -> state
  
 
-loop :: SDL.Renderer -> Character -> SDL.Window -> IO ()
-loop r (Character texture state) window  = do
+loop :: SDL.Renderer -> Character -> Int -> SDL.Window -> IO ()
+loop r (Character texture state) seed window  = do
   poll <- SDL.pollEvents
   -- If elapsed time > 100, update game
   let quit = elem SDL.QuitEvent $ map SDL.eventPayload poll
   SDL.rendererDrawColor r $= V4 maxBound maxBound maxBound maxBound
   SDL.clear r
-  -- Update next sprite animation
+  -- Update next sprite animation -- Update draw position
   ((sprite, state', index, game), xs) <- processEvents' state poll
-  -- Update draw position
-  
-  -- putStrLn (show $ (index, (x', y')))
-
-  -- Get texture and sprite from current state
+ 
+      -- Get texture and sprite from current state
   let (texture', rects) = runSprite sprite state' 
       toDraw = map (\(x, y) -> P $ V2 x y) xs
       rect = Just $ rects !! index
+      -- Spawn coin from body
+      ((coinx, coiny), seed') = spawnCoin xs seed
   -- Draw sprite at current position
   sequence $ map (\pv2 -> renderTexture r texture pv2 rect) toDraw
+
+  -- Debug printing
   sequence $ map (\(g, f) -> (print (g, f))) xs
+  print ((coinx, coiny), seed)
+
+  -- Draw
+  renderTexture r texture (P $ V2 70 70) (Just $ 
+          SDL.Rectangle (P $ V2 100 100) (V2 50 50) ) 
+
   SDL.present r
-  unless quit $ loop r (Character texture ((sprite, state', index, game), xs)) window
+  unless quit $ loop r (Character texture ((sprite, state', index, game), xs)) seed' window
 
-
-updateBody :: CharSTATE -> Body -> Body
+-- to fix : doesn't work with length == 1
+updateBody :: Direction -> Body -> Body
 updateBody charSTATE [] = []
 updateBody charSTATE (x:xs) = xs ++ ( map f $ drop (length xs - 1) xs )
       where f = \(a, b) -> case charSTATE of 
@@ -146,6 +154,27 @@ updateBody charSTATE (x:xs) = xs ++ ( map f $ drop (length xs - 1) xs )
                                 D -> (a, b-50)
                                 L -> (a-50, b)
                                 R -> (a+50, b)
+
+freeCells :: Body -> Body
+freeCells body =  [   (height*50, width*50) | 
+                      height <- [0..16], 
+                      width <- [0..16], 
+                      bodyXY <- body,
+                      fst bodyXY /= height*50 || snd bodyXY /= width*50  ]
+
+spawnCoin :: Body -> Int -> ((CInt, CInt), Int)
+spawnCoin body seed = 
+  let availableCells = freeCells body 
+      l = length availableCells
+      (st, x) = runState (myRandom $ l) seed
+      (st', y) = runState (myRandom $ l) st
+  in (availableCells !! ((x + y) `mod` l), st')
+
+myRandom :: Int -> State Int Int
+myRandom availableCells = do 
+  b <- ST.get 
+  put (b * 3 + 9)
+  return ((b * 3 + 9) `mod` availableCells)
 
 main :: IO ()
 main = do
@@ -166,13 +195,14 @@ main = do
   }
   SDL.rendererDrawColor renderer $= V4 maxBound maxBound maxBound maxBound
   --
-  -- Load image
+  -- Load Texture
   spriteSheetTexture <- loadTexture renderer "../assets/SF.bmp"
-
+  
   let draw = renderTexture renderer
-  let game = Game 0
-  let spriteSize = V2 50 50
-      --Define idle clips
+      game = Game 0
+      seed = 2
+      -- Load Snake
+      spriteSize = V2 50 50
       clip1 = SDL.Rectangle (P (V2 0 10)) spriteSize
       clip2 = SDL.Rectangle (P (V2 50 10)) spriteSize
       clip3 = SDL.Rectangle (P (V2 100 10)) spriteSize
@@ -182,8 +212,11 @@ main = do
         _ -> (spriteSheetTexture, [clip1, clip2, clip3, clip4])
 
 
+
   -- loop renderer (True, (0,0)) window 
-  loop renderer (Character spriteSheetTexture ((sprite, U, 0, game), [(50, 50),(100, 100)])) window
+  loop renderer 
+    (Character spriteSheetTexture ((sprite, U, 0, game), [(50, 50),(100, 100)])) 
+         seed window
 
 
   SDL.destroyWindow window
