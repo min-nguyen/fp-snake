3463afc0ca2b9245a99f4793d2c4dae6db1b8a1e
Minh <minhnguyen1995@googlemail.com>
2017-09-11 18:44:06 +0100

    .

--- src/Min.hs
+++ src/Min.hs
@@ -20,7 +20,8 @@ import SDL.Vect
 import qualified SDL as SDL
 import System.FilePath.Posix
 import Control.Monad.State as ST
-
+import qualified SDL.Raw.Timer as Raw
+import qualified SDL.Raw.Types as Raw
 
 screenWidth, screenHeight :: CInt
 (screenWidth, screenHeight) = (640, 480)
@@ -33,28 +34,41 @@ screenWidth, screenHeight :: CInt
 -- SDL.Texture, (Width, Height)
 data Texture = Texture SDL.Texture (V2 CInt)
 data Character = Character Texture CharState 
-
+data Game = Game { getTime :: Int } 
+type Time = Int
 
 newtype Sprite = Sprite {runSprite :: CharSTATE -> (Texture, [SDL.Rectangle CInt])} 
 type SpriteIndex = Int
-data CharSTATE = IDLE
-type CharState = ((Sprite, CharSTATE, SpriteIndex), (CInt, CInt))
+data CharSTATE = U | D | L | R 
+type CharState = ((Sprite, CharSTATE, SpriteIndex, Game), (CInt, CInt))
 type CharPosition = (CInt, CInt)
+type ElapsedTime = Int
 
-updatePosition :: Char -> State CharState CharPosition
-updatePosition c = do
-  ((sprite, state, index), (x, y)) <- ST.get
+updateState :: Char -> State CharState CharPosition
+updateState c = do
+  ((sprite, state, index, game), (x, y)) <- ST.get
   let spriteIndexSize = length . snd $ runSprite sprite state
       nextIndex       = if index >= (spriteIndexSize - 1) 
                         then 0 
                         else index + 1
   case c of 
-    'a' -> put((sprite, state, nextIndex), (x-5, y))
-    'd' -> put((sprite, state, nextIndex), (x+5, y))
-    's' -> put((sprite, state, nextIndex), (x, y-5))
-    'w' -> put((sprite, state, nextIndex), (x, y+5))
-  (_, (st, xy)) <-  ST.get
-  traceShow (st, xy) ( return (st, xy))
+    'a' -> put((sprite, L, nextIndex, game), (x, y))
+    'd' -> put((sprite, R, nextIndex, game), (x, y))
+    's' -> put((sprite, D, nextIndex, game), (x, y))
+    'w' -> put((sprite, U, nextIndex, game), (x, y))
+
+  (_, (x', y')) <-  ST.get
+  return (x', y')
+
+updatePosition :: Int -> State CharState CharPosition 
+updatePosition = do   
+  let elapsedTime = (fromIntegral ticks) - (getTime game)
+      game' = if elapsedTime > 500 
+              then Game $ fromIntegral ticks
+              else game
+  (x', y') =  if getTime game' /= getTime game 
+              then (50, 50)
+              else (0, 0)
 
 
 loadTexture :: SDL.Renderer -> FilePath -> IO Texture
@@ -79,35 +93,45 @@ renderTexture r (Texture t size) xy clip =
   
 processEvents' :: CharState -> [SDL.Event] -> IO (CharState)
 processEvents' state events = do 
+    ticks <- Raw.getTicks
     let x = foldr (\ev st -> let eventType = SDL.eventPayload ev in case eventType of 
-                    SDL.KeyboardEvent eventType -> processKeyboard' st $ SDL.keyboardEventKeysym eventType
+                    SDL.KeyboardEvent eventType -> 
+                      processKeyboard' st (SDL.keyboardEventKeysym eventType) 
+                        (fromIntegral ticks)
                     _ -> st) state events 
     return x
 
-processKeyboard' :: CharState -> SDL.Keysym -> CharState
-processKeyboard' state keySym = 
+processKeyboard' :: CharState -> SDL.Keysym -> Int -> CharState
+processKeyboard' state keySym ticks = 
   case SDL.keysymKeycode keySym of
-    SDL.KeycodeUp   -> execState (updatePosition 's') state
-    SDL.KeycodeLeft -> execState (updatePosition 'a') state
-    SDL.KeycodeRight-> execState (updatePosition 'd') state
-    SDL.KeycodeDown -> execState (updatePosition 'w') state
+    SDL.KeycodeUp   -> execState (updateState 's') state 
+    SDL.KeycodeLeft -> execState (updateState 'a') state 
+    SDL.KeycodeRight-> execState (updateState 'd') state 
+    SDL.KeycodeDown -> execState (updateState 'w') state 
     _ -> state
 
+
+
 loop :: SDL.Renderer -> Character -> SDL.Window -> IO ()
-loop r (Character texture state) window = do
+loop r (Character texture state) window  = do
   poll <- SDL.pollEvents
+  -- If elapsed time > 100, update game
   let quit = elem SDL.QuitEvent $ map SDL.eventPayload poll
   SDL.rendererDrawColor r $= V4 maxBound maxBound maxBound maxBound
   SDL.clear r
-  -- Update next sprite animation, index and draw position
-  ((sprite, state, index), (x, y)) <- processEvents' state poll
-  putStrLn (show $ (index, (x, y)) )
+  -- Update next sprite animation
+  ((sprite, state, index, game), (x, y)) <- processEvents' state poll
+  -- Update draw position
+  
+  -- putStrLn (show $ (index, (x', y')))
+
   -- Get texture and sprite from current state
   let (texture', rects) = runSprite sprite state 
   -- Draw sprite at current position
   renderTexture r texture (P $ V2 x y) (Just $ rects !! index)
   SDL.present r
-  unless quit $ loop r (Character texture ((sprite, state, index), (x, y))) window
+  unless quit $ loop r (Character texture ((sprite, state, index, game), (x, y))) window
+
 
 
 
@@ -134,8 +158,8 @@ main = do
   spriteSheetTexture <- loadTexture renderer "../assets/SF.bmp"
 
   let draw = renderTexture renderer
-
-  let spriteSize = V2 50 90
+  let game = Game 0
+  let spriteSize = V2 50 50
       --Define idle clips
       clip1 = SDL.Rectangle (P (V2 0 10)) spriteSize
       clip2 = SDL.Rectangle (P (V2 50 10)) spriteSize
@@ -143,11 +167,11 @@ main = do
       clip4 = SDL.Rectangle (P (V2 150 10)) spriteSize
       --Define character sprite loading functions
       sprite = Sprite $ \state -> case state of 
-        IDLE -> (spriteSheetTexture, [clip1, clip2, clip3, clip4])
+        _ -> (spriteSheetTexture, [clip1, clip2, clip3, clip4])
 
 
   -- loop renderer (True, (0,0)) window 
-  loop renderer (Character spriteSheetTexture ((sprite, IDLE, 0), (50, 50))) window
+  loop renderer (Character spriteSheetTexture ((sprite, U, 0, game), (50, 50))) window
 
 
   SDL.destroyWindow window
